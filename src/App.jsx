bde0d826bcc4b2c9242f36f691c35e4464d16de2
corynko <ethankotel@gmail.com>
2025-04-07 23:42:45 -0600

    fleshed out copy on about page, created infrastructure to display paper cards held in aboutCardArray.jsx, many updates to associated css modules, minor changes to clean up imports in welcome.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import '@mantine/core/styles.css';
 
 import { MantineProvider } from '@mantine/core';
+import { Navbar } from './components/NavBar/navBar';
 import { BackgroundProvider } from './contexts/backgroundContext';
 
 import './styles.css';
@@ -13,6 +14,7 @@ export default function App() {
   return (
     <MantineProvider theme={theme}>
       <BackgroundProvider>
+        <Navbar />
         <RouterProvider router={Router} />
       </BackgroundProvider>
     </MantineProvider>
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,20 +1,63 @@
-import { useEffect } from 'react';
-import { useMantineColorScheme } from '@mantine/core';
+import { useEffect, useRef } from 'react';
+import { motion } from 'motion/react';
+import { Container, Paper, Text, Title, useMantineColorScheme } from '@mantine/core';
 import lightBg from '../../assets/png/EKP_CIR-67.jpg';
 import darkBg from '../../assets/png/EKP_SGV-32.jpg';
 import { useBackground } from '../../contexts/backgroundContext';
 import { usePrimaryColor } from '../../theme/usePrimaryColor';
-import { Navbar } from '../NavBar/navBar';
+import { AboutCardArray } from './aboutCardArray';
 import classes from './About.module.css';
-
-import '../../hover.css';
 
 export function About() {
   const { colorScheme } = useMantineColorScheme();
   const isDark = colorScheme === 'dark';
+  const isInvert = colorScheme !== 'dark';
   const navColor = usePrimaryColor(9, 3);
+  const invertColor = usePrimaryColor(1, 9);
 
   const { setBackgrounds } = useBackground();
+
+  const paperRef = useRef(null);
+
+  const headerVariants = {
+    start: {
+      opacity: 0,
+    },
+    finish: {
+      opacity: 1,
+      transition: {
+        delay: 0.2,
+        duration: 0.75,
+        ease: 'easeInOut',
+      },
+    },
+  };
+  const paperContainerVariants = {
+    start: {
+      opacity: 0,
+    },
+    finish: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.4,
+        delayChildren: 0.5,
+      },
+    },
+  };
+  const paperVariants = {
+    start: {
+      opacity: 0,
+      y: 20,
+    },
+    finish: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.8,
+        ease: 'easeInOut',
+      },
+    },
+  };
 
   useEffect(() => {
     setBackgrounds({
@@ -25,8 +68,51 @@ export function About() {
 
   return (
     <>
-      <Navbar />
-      <div className={classes.content}></div>
+      <div className={classes.content}>
+        <motion.div variants={headerVariants} initial="start" animate="finish">
+          <Title className={classes.titleLocation}>
+            <Text className={classes.title} component="span">
+              about
+            </Text>
+            <Text className={classes.titleSpacer} component="span">
+              ||
+            </Text>
+            <Text className={classes.title} component="span">
+              us
+            </Text>
+          </Title>
+        </motion.div>
+        <motion.div variants={paperContainerVariants} initial="start" animate="finish">
+          <Container className={classes.paperContainer}>
+            {AboutCardArray.map(({ title, text }, index) => (
+              <motion.div
+                key={index}
+                variants={paperVariants}
+                style={{
+                  backdropFilter: 'blur(0px)',
+                }}
+                whileInView={{
+                  backdropFilter: 'blur(4px)',
+                  backgroundColor: isDark ? '#00000044' : '#ffffff22',
+                  transition: {
+                    duration: 0.8,
+                    ease: 'easeInOut',
+                  },
+                }}
+              >
+                <Paper className={classes.aboutPaper}>
+                  <Title className={classes.paperHeadline} c={navColor}>
+                    {title}
+                  </Title>
+                  <Text className={classes.paperText} c={navColor}>
+                    {text}
+                  </Text>
+                </Paper>
+              </motion.div>
+            ))}
+          </Container>
+        </motion.div>
+      </div>
     </>
   );
 }
